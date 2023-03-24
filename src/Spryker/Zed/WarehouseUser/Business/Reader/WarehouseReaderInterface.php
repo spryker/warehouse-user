ff3e02ec3c579244a0e00ef91224015778b0ee1f
Sergey Samoylov <43746999+zssamoylov@users.noreply.github.com>
2023-03-24 16:46:01 +0200

    CC-20611 Single order picking items in picklist (#9916)
    CC-20611 Single-order picking - Picking items in the Picklist

--- src/Spryker/Zed/WarehouseUser/Business/Reader/WarehouseReaderInterface.php
+++ src/Spryker/Zed/WarehouseUser/Business/Reader/WarehouseReaderInterface.php
@@ -5,16 +5,17 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\WarehouseUser\Business\Expander;
+namespace Spryker\Zed\WarehouseUser\Business\Reader;
 
 use ArrayObject;
+use Generated\Shared\Transfer\StockCollectionTransfer;
 
-interface WarehouseUserAssignmentExpanderInterface
+interface WarehouseReaderInterface
 {
     /**
      * @param \ArrayObject<\Generated\Shared\Transfer\WarehouseUserAssignmentTransfer> $warehouseUserAssignmentTransfers
      *
-     * @return \ArrayObject<\Generated\Shared\Transfer\WarehouseUserAssignmentTransfer>
+     * @return \Generated\Shared\Transfer\StockCollectionTransfer
      */
-    public function expandWarehouseUserAssignmentTransfersWithWarehouses(ArrayObject $warehouseUserAssignmentTransfers): ArrayObject;
+    public function getStockCollection(ArrayObject $warehouseUserAssignmentTransfers): StockCollectionTransfer;
 }
